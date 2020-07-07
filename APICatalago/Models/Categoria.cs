13422eda82cb21548772629adb70f8e3eda306f9
Eduardo Claudino <eduardoclaudino@gmail.com>
2020-07-06 22:44:11 -0300

    Migrations

--- APICatalago/Models/Categoria.cs
+++ APICatalago/Models/Categoria.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,8 +14,13 @@ namespace APICatalago.Models
         {
             Produtos = new Collection<Produto>();
         }
+
         public int CategoriaId { get; set; }
+        [Required]
+        [MaxLength(80)]
         public string Nome { get; set; }
+        [Required]
+        [MaxLength(300)]
         public string ImagemUrl { get; set; }
         public ICollection<Produto> Produtos { get; set; }
     }
--- APICatalago/Models/Produto.cs
+++ APICatalago/Models/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,16 @@ namespace APICatalago.Models
     public class Produto
     {
         public int ProdutoId { get; set; }
+        [Required]
+        [MaxLength(80)]
         public string Nome { get; set; }
+        [Required]
+        [MaxLength(300)]
         public string Descricao { get; set; }
+        [Required]
         public decimal Preco { get; set; }
+        [Required]
+        [MaxLength(500)]
         public string ImagemUrl { get; set; }
         public float Estoque { get; set; }
         public DateTime DataCadastro { get; set; }
